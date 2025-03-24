1a1c9860915ddd995c2aa38d86503f52df45f968
glarkham <gwlarkham@gmail.com>
2025-03-23 22:51:31 -0400

    Refactor makefile build system

--- make/define.mk
+++ make/define.mk
@@ -1,11 +1,17 @@
+###############################################################################
 # Functions
+###############################################################################
 read_json=$(shell python -c "import json; print(json.load(open('$(1)'))['$(2)'])" 2>/dev/null)
 
+###############################################################################
 # Sentinels
+###############################################################################
 SENTINEL_DIR=tmp
 SENTINEL_EXT=.sentinel
 
+###############################################################################
 # Dotnet
+###############################################################################
 ## Configuration
 DOTNET?=dotnet
 DOTNET_VSMOD_PACKAGE_NAME?=VintageStory.Mod.Templates
@@ -14,28 +20,46 @@ DOTNET_VSMODE=$(DOTNET) new vsmod
 DOTNET_VSMOD_INSTALL_SENTINEL=\
     $(SENTINEL_DIR)/dotnet-vsmod-install$(SENTINEL_EXT)
 
+###############################################################################
 # Project
+###############################################################################
 ## Configuration
 PROJECT_NAME?=HotbarScrollControl
 PROJECT_BUILD_PROFILE?=Debug
 ## Definitions
 ### Directories
+#### Common
 PROJECT_DIR=src/$(PROJECT_NAME)
 PROJECT_RELEASES_DIR=$(PROJECT_DIR)/Releases
-PROJECT_SRC_DIR=$(PROJECT_DIR)/$(PROJECT_NAME)
-PROJECT_BUILD_DIR=\
-    $(PROJECT_SRC_DIR)/bin/$(PROJECT_BUILD_PROFILE)/net7.0
+#### Cake
 PROJECT_CAKE_SRC_DIR=$(PROJECT_DIR)/CakeBuild
 PROJECT_CAKE_BUILD_DIR=\
-    $(PROJECT_CAKE_SRC_DIR)/bin/$(PROJECT_BUILD_PROFILE)/net7.0
+    $(PROJECT_CAKE_BIN_DIR)/$(PROJECT_BUILD_PROFILE)/net7.0
+#### Mod
+PROJECT_SRC_DIR=$(PROJECT_DIR)/$(PROJECT_NAME)
+PROJECT_BUILD_DIR=\
+    $(PROJECT_SRC_DIR)/bin/$(PROJECT_BUILD_PROFILE)/Mods/mod
 ### Version
 PROJECT_VERSION=$(call read_json,$(PROJECT_SRC_DIR)/modinfo.json,version)
+PROJECT_MODID=$(call read_json,$(PROJECT_SRC_DIR)/modinfo.json,modid)
 ### Recipes
+#### Create
 PROJECT_CREATE_SENTINEL=\
     $(SENTINEL_DIR)/project-create-$(PROJECT_NAME)$(SENTINEL_EXT)
 PROJECT_CREATE_PREREQUISITES=\
     $(DOTNET_VSMOD_INSTALL_SENTINEL)
-PROJECT_BUILD_PREREQUISITES=\
-    $(PROJECT_CREATE_SENTINEL)
+#### Build
+PROJECT_BUILD_ALL_PREREQUISITES=\
+    project-target-cake-all\
+    project-target-mod-all\
+    project-target-release-all
+PROJECT_BUILD_CLEAN_PREREQUISITES=\
+    project-target-cake-clean\
+    project-target-mod-clean\
+    project-target-release-clean
+#### Run
 PROJECT_RUN_PREREQUISITES=\
     project-build-all
+#### Target
+PROJECT_TARGET_PREREQUISITES=\
+    $(PROJECT_CREATE_SENTINEL)
--- make/project.mk
+++ make/project.mk
@@ -1,10 +1,8 @@
-.PHONY: project-destroy
-project-destroy:
-	rm -drf $(PROJECT_DIR)
-	rm -f $(PROJECT_CREATE_SENTINEL)
-
-.PHONY: project-create
-project-create: $(PROJECT_CREATE_SENTINEL)
+###############################################################################
+# Create
+###############################################################################
+.PHONY: project-create-all
+project-create-all: $(PROJECT_CREATE_SENTINEL)
 $(PROJECT_CREATE_SENTINEL): $(PROJECT_CREATE_PREREQUISITES)
 	$(shell mkdir -p $(dir $@))
 	$(DOTNET_VSMODE) \
@@ -16,30 +14,62 @@ $(PROJECT_CREATE_SENTINEL): $(PROJECT_CREATE_PREREQUISITES)
 	rm -f $(PROJECT_DIR)/build.sh
 	touch $@
 
+.PHONY: project-create-clean
+project-create-clean:
+	rm -drf $(PROJECT_DIR)
+	rm -f $(PROJECT_CREATE_SENTINEL)
+
+###############################################################################
+# Build
+###############################################################################
 .PHONY: project-build-all
-project-build-all: $(PROJECT_BUILD_PREREQUISITES)
-	$(DOTNET) build -c $(PROJECT_BUILD_PROFILE) \
-		$(PROJECT_CAKE_SRC_DIR)/CakeBuild.csproj
-	$(DOTNET) build -c $(PROJECT_BUILD_PROFILE) \
-		$(PROJECT_SRC_DIR)/$(PROJECT_NAME).csproj
-	$(DOTNET) run --project $(PROJECT_CAKE_SRC_DIR)/CakeBuild.csproj
+project-build-all: $(PROJECT_BUILD_ALL_PREREQUISITES)
 
 .PHONY: project-build-clean
-project-build-clean: $(PROJECT_BUILD_PREREQUISITES)
-	$(DOTNET) clean -c $(PROJECT_BUILD_PROFILE) \
-		$(PROJECT_SRC_DIR)/$(PROJECT_NAME).csproj
-	$(DOTNET) clean -c $(PROJECT_BUILD_PROFILE) \
-		$(PROJECT_CAKE_SRC_DIR)/CakeBuild.csproj
-	rm -drf $(PROJECT_RELEASES_DIR)
+project-build-clean: $(PROJECT_BUILD_CLEAN_PREREQUISITES)
 
+###############################################################################
+# Run
+###############################################################################
 .PHONY: project-run-client
 project-run-client: $(PROJECT_RUN_PREREQUISITES)
 	$(VINTAGE_STORY)/Vintagestory \
 		--tracelog \
-		--addModPath $(PROJECT_DIR)/bin/$(PROJECT_BUILD_PROFILE)/Mods
+		--addModPath $(PROJECT_DIR)/bin/Debug/Mods
 
 .PHONY: project-run-server
 project-run-server: $(PROJECT_RUN_PREREQUISITES)
 	$(VINTAGE_STORY)/VintagestoryServer \
 		--tracelog \
-		--addModPath $(PROJECT_DIR)/bin/$(PROJECT_BUILD_PROFILE)/Mods
+		--addModPath $(PROJECT_DIR)/bin/Debug/Mods
+
+###############################################################################
+# Targets
+###############################################################################
+## Cake
+.PHONY: project-target-cake-all
+project-target-cake-all: $(PROJECT_TARGET_PREREQUISITES)
+	$(DOTNET) build -c Debug $(PROJECT_CAKE_SRC_DIR)/CakeBuild.csproj
+
+.PHONY: project-target-cake-clean
+project-target-cake-clean:
+	$(DOTNET) clean -c Debug $(PROJECT_CAKE_SRC_DIR)/CakeBuild.csproj
+
+## Mod
+.PHONY: project-target-mod-all
+project-target-mod-all: $(PROJECT_TARGET_PREREQUISITES)
+	$(DOTNET) build -c Debug $(PROJECT_SRC_DIR)/$(PROJECT_NAME).csproj
+
+.PHONY: project-target-mod-clean
+project-target-mod-clean:
+	$(DOTNET) clean -c Debug $(PROJECT_SRC_DIR)/$(PROJECT_NAME).csproj
+
+## Release
+.PHONY: project-target-release-all
+project-target-release-all: $(PROJECT_TARGET_PREREQUISITES)
+	$(DOTNET) run --project $(PROJECT_CAKE_SRC_DIR)/CakeBuild.csproj
+
+.PHONY: project-target-release-clean
+project-target-release-clean:
+	$(DOTNET) clean -c Release $(PROJECT_SRC_DIR)/$(PROJECT_NAME).csproj
+	rm -drf $(PROJECT_RELEASES_DIR)
