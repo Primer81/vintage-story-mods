0860219de650bbbc9c3e1664d5516b22b9dda175
glarkham <gwlarkham@gmail.com>
2025-03-23 23:14:49 -0400

    Skip defining VINTAGE_STORY if already defined

--- make/env.mk
+++ make/env.mk
@@ -1,13 +1,15 @@
-ifeq ($(OS),Windows_NT)
-    ifndef APPDATA
-        $(error APPDATA is undefined)
+ifndef VINTAGE_STORY
+    ifeq ($(OS),Windows_NT)
+        ifndef APPDATA
+            $(error APPDATA is undefined. Needed to define VINTAGE_STORY)
+        endif
+        export VINTAGE_STORY=$(APPDATA)/Vintagestory
+    else
+        ifndef HOME
+            $(error HOME is undefined. Needed to define VINTAGE_STORY)
+        endif
+        export VINTAGE_STORY=$(HOME)/ApplicationData/vintagestory
     endif
-    export VINTAGE_STORY=$(APPDATA)/Vintagestory
-else
-    ifndef HOME
-        $(error HOME is undefined)
-    endif
-    export VINTAGE_STORY=$(HOME)/ApplicationData/vintagestory
 endif
 
 PYTHON_CHECK := $(shell which python3 2>/dev/null || which python 2>/dev/null)
