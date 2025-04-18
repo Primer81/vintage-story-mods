ea71d3ed15507677e427ac1dc1d3912d88f5c103
glarkham <gwlarkham@gmail.com>
2025-04-17 23:41:02 -0400

    Improve sneak toggling by making it coincide with sneak interactions

--- src/ToggleSneak/ToggleSneak/ToggleSneakModSystem.cs
+++ src/ToggleSneak/ToggleSneak/ToggleSneakModSystem.cs
@@ -3,64 +3,107 @@ using Vintagestory.API.Server;
 using Vintagestory.API.Config;
 using Vintagestory.API.Common;
 using Vintagestory;
+using Vintagestory.Client.NoObf;
 using System;
+using HarmonyLib;
 
 namespace ToggleSneak;
 
 public class ToggleSneakModSystem : ModSystem
 {
-    private ICoreClientAPI clientApi;
+    static public ICoreClientAPI ClientApi;
+
+    static private Harmony harmony;
+    static private bool sneakToggledOn;
 
     private bool triggerOnUpAlsoOriginal = false;
-    private bool sneakKeyIsPressed = false;
-    static private bool sneakToggledOn = false;
 
     public override void StartClientSide(ICoreClientAPI api)
     {
-        clientApi = api;
-        triggerOnUpAlsoOriginal =
-            clientApi.Input.HotKeys["sneak"].TriggerOnUpAlso;
-        clientApi.Input.HotKeys["sneak"].TriggerOnUpAlso = true;
-        clientApi.Input.HotKeys["sneak"].Handler +=
-            OnToggleSneakHotkey;
-         clientApi.Event.RegisterGameTickListener(
-            OnGameTickCheckSneakToggle, 5);
+        ClientApi = api;
+        // Initial static state defaults
+        sneakToggledOn = false;
+        // Enable sneak toggle
+        {
+            triggerOnUpAlsoOriginal =
+                ClientApi.Input.HotKeys["sneak"].TriggerOnUpAlso;
+            ClientApi.Input.HotKeys["sneak"].TriggerOnUpAlso = false;
+        }
+        // Apply harmony patches
+        {
+            harmony = new Harmony(Mod.Info.ModID);
+            harmony.PatchCategory(Mod.Info.ModID);
+        }
     }
 
     public override void Dispose()
     {
+        // Unpatch if possible
+        {
+            harmony?.UnpatchAll(Mod.Info.ModID);
+            harmony = null;
+        }
         // Restore hotkey
         {
-            clientApi.Input.HotKeys["sneak"].TriggerOnUpAlso =
+            ClientApi.Input.HotKeys["sneak"].TriggerOnUpAlso =
                 triggerOnUpAlsoOriginal;
-            clientApi.Input.HotKeys["sneak"].Handler -=
-                OnToggleSneakHotkey;
         }
         base.Dispose();
-    }
-
-    private bool OnToggleSneakHotkey(KeyCombination keyComb)
-    {
-        bool isPressed = clientApi.Input.KeyboardKeyState[keyComb.KeyCode];
-        if ((sneakKeyIsPressed == false) && (isPressed == true))
-        {
-            sneakKeyIsPressed = true;
-            ToggleSneak();
-        }
-        if ((sneakKeyIsPressed == true) && (isPressed == false))
-        {
-            sneakKeyIsPressed = false;
-        }
-        return true;
-    }
-
-    private void OnGameTickCheckSneakToggle(float dt)
-    {
-        clientApi.World.Player.Entity.Controls.Sneak = sneakToggledOn;
     }
 
     public static void ToggleSneak()
     {
         sneakToggledOn = !sneakToggledOn;
+    }
+
+    public static bool IsSneakToggledOn()
+    {
+        return sneakToggledOn;
+    }
+
+    public static int GetSneakKeyCode()
+    {
+        return ClientApi.Input.HotKeys["sneak"].CurrentMapping.KeyCode;
+    }
+}
+
+[HarmonyPatchCategory("togglesneak")]
+internal static class Patches
+{
+    private const int ACTION_FLAG_TOGGLE_SNEAK_OFF = unchecked((int)0x80000000);
+
+    [HarmonyPrefix()]
+    [HarmonyPatch(typeof(ClientMain), "OnKeyDown")]
+    public static bool Before_ClientMain_OnKeyDown(
+        ClientMain __instance,
+        KeyEvent args)
+    {
+        bool runOriginal = true;
+        if (args.KeyCode == ToggleSneakModSystem.GetSneakKeyCode())
+        {
+            ToggleSneakModSystem.ToggleSneak();
+            if (ToggleSneakModSystem.IsSneakToggledOn() == false)
+            {
+                args.KeyCode |= ACTION_FLAG_TOGGLE_SNEAK_OFF;
+                __instance.OnKeyUp(args);
+                runOriginal = false;
+            }
+        }
+        return runOriginal;
+    }
+
+    [HarmonyPrefix()]
+    [HarmonyPatch(typeof(ClientMain), "OnKeyUp")]
+    public static bool Before_ClientMain_OnKeyUp(ref KeyEvent args)
+    {
+        bool runOriginal = true;
+        int actualKeyCode = args.KeyCode & ~ACTION_FLAG_TOGGLE_SNEAK_OFF;
+        bool shouldToggleSneakOff = (args.KeyCode & ACTION_FLAG_TOGGLE_SNEAK_OFF) != 0;
+        if (actualKeyCode == ToggleSneakModSystem.GetSneakKeyCode())
+        {
+            runOriginal = shouldToggleSneakOff;
+        }
+        args.KeyCode = actualKeyCode;
+        return runOriginal;
     }
 }
